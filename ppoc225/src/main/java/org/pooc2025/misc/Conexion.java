f8875d639281f339929d88feaee73cdd1a240163
Gerardo Gomez <93677606+Javalopeer@users.noreply.github.com>
2025-06-16 19:57:27 -0600

    FirstUpld

---
+++ ppoc225/src/main/java/org/pooc2025/misc/Conexion.java
@@ -0,0 +1,35 @@
+package org.pooc2025.misc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mysql://srv863.hstgr.io:3306/u484426513_ppoc225";
+    private static final String USER = "u484426513_ppoc225";
+    private static final String PASSWORD = "jXc7w:|7Gy";
+
+    public static Connection obtenerConexion() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("❌ Error al conectar: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection con = obtenerConexion();
+
+        if (con != null) {
+            System.out.println("Conectado a la base de datos.");
+
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al conectar: " + e.getMessage());
+            }
+        }
+    }
+}
